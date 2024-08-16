8086710f3dc84ec6b1bcd903915e4cb9f822c590
Viet An <luongvietan.231123@gmail.com>
2024-08-16 23:42:36 +0700

    register

--- src/components/register/index.jsx
+++ src/components/register/index.jsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
+import CommonForm from "../common-form";
+
+const initialRegisterData = {
+    name: "",
+    email: "",
+    password: "",
+};
+  
 export default function RegisterComponent() {
+    const [registerData, setRegisterData] = useState(initialRegisterData)
+    function handleRegisterSubmit(event) {
+        event.preventDefault();
+        console.log(registerData);
+    }
     return <div>
         <h1>Register page/component
+            <CommonForm
+                formControls={registerElements}
+                formData={registerData}
+                setFormData={setRegisterData}
+                buttonText={'Register'}
+                onHandleSubmit={handleRegisterSubmit}
+            />
         </h1></div>
 }
